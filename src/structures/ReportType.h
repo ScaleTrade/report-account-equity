d7637b21fbc05cefb0a69a94aa63d1af0ae5d49b
Iurii Dubinin <yuradubinin100@gmail.com>
2026-04-20 13:11:34 +0300

    upd

--- src/structures/ReportType.h
+++ src/structures/ReportType.h
@@ -1,18 +1,18 @@
 #pragma once
 
 enum class ReportType {
-    NONE,               // report without filter
-    RANGE,              // report filter with time range
-    DAILY,              // report filter by one day
-    ACCOUNT,            // report filter by account
-    SYMBOL,             // report filter by account
-    GROUP,              // report filter with group mask
-    RANGE_GROUP,        // report filter one day and group mask
-    DAILY_GROUP,        // report filter one day and group mask
-    RANGE_ACCOUNT,      // report filter with time range and account
-    DAILY_ACCOUNT,      // report filter by one day and account
-    RANGE_SYMBOL,       // report filter with time range and account
-    DAILY_SYMBOL,       // report filter by one day and account
-    RANGE_GROUP_SYMBOL, // report filter with time range and group and symbols
-    DAILY_GROUP_SYMBOL  // report filter by one day and groups and symbols
+    None,             // report without filter
+    Range,            // report filter with time range
+    Daily,            // report filter by one day
+    Account,          // report filter by account
+    Symbol,           // report filter by account
+    Group,            // report filter with group mask
+    RangeGroup,       // report filter one day and group mask
+    DailyGroup,       // report filter one day and group mask
+    RangeAccount,     // report filter with time range and account
+    DailyAccount,     // report filter by one day and account
+    RangeSymbol,      // report filter with time range and account
+    DailySymbol,      // report filter by one day and account
+    RangeGroupSymbol, // report filter with time range and group and symbols
+    DailyGroupSymbol  // report filter by one day and groups and symbols
 };
